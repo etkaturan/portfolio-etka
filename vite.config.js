802342194d892528cee5a3bc3c89c026a5c0e252
etkaturan <metkaturanua@gmail.com>
2025-01-15 04:33:06 +0100

    commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Portfolio/", // Replace 'Portfolio' with your repo name
+  base: "/portfolio-etka/", // Replace 'portfolio-etka' with your repo name
   plugins: [react()],
 });
